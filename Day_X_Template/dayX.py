88141c2e0d2efdd861482e687f8573d561cc5ffd
Jon White <white.jon@gmail.com>
2021-12-02 09:54:09 +0000

    Add template code

---
+++ Day_X_Template/dayX.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+
+dirname = os.path.dirname(__file__)
+inputfile = os.path.join(dirname, 'input.txt')
+
+def part1(data):
+    print("Part 1:")
+
+    return
+
+
+def part2(data):
+    print("Part 2:")
+
+    return
+
+
+def main():
+
+    currentDay = os.path.basename(__file__).split('.')[0]
+    print(currentDay)
+
+    with open(inputfile) as f:
+        data = f.read().splitlines()
+
+        part1(data)
+        #part2(data)
+
+
+if __name__ == "__main__":
+    main()
