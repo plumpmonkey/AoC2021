3766926260e2ec587f80bd6fb4ad23bd64ecf70a
Jon White <white.jon@gmail.com>
2021-12-04 14:26:01 +0000

    Day 3 Part-2 Complete

--- Day_3/day3.py
+++ Day_3/day3.py
@@ -5,9 +5,12 @@ from collections import defaultdict
 
 import os
 
+import numpy
+import numpy as np
+
 dirname = os.path.dirname(__file__)
-#inputfile = os.path.join(dirname, 'input.txt')
-inputfile = os.path.join(dirname, 'sample.txt')
+inputfile = os.path.join(dirname, 'input.txt')
+#inputfile = os.path.join(dirname, 'sample.txt')
 
 def part1(data, numLines, numBits):
     print("Part 1:")
@@ -37,6 +40,7 @@ def part1(data, numLines, numBits):
                 gamma += str(0)
                 epsilon += str(1)
 
+    print(f"  Ones-Count = {ones_count}")
     print(f"  Gamma = {int(gamma,2)}")
     print(f"  Epsilon = {int(epsilon, 2)}")
     print(f"  Power Consumption = {int(gamma,2) * int(epsilon, 2)}")
@@ -47,24 +51,75 @@ def part1(data, numLines, numBits):
 def part2(data, numLines, numBits ):
     print("Part 2:")
 
-    # Default the dictionary entries in order
-    ones_count = {}
-    for index, value in enumerate(data[0]):
-        ones_count[index] = 0
+    ## This time use NumPy to store the numbers
+    num_list = np.array([list(i) for i in data], dtype=int)
 
-    # Loop through the data counting the ones
-    for line in data:
-        for index, value in enumerate(line):
-           if value == '1':
-                ones_count[index] += 1
+    oxygen = analyse_list("oxygen", numBits, num_list)
+    co2 = analyse_list("co2", numBits, num_list)
+    life_support = oxygen * co2
 
-    # Take a copy of our data.
-    oxygen_data = data
+    print(f"  LifeSupport = {life_support}")
 
-    # Check we have more than one entry left in our data
-    #if(len(oxygen_data) > 1):
 
     return
+
+
+def analyse_list(mode, numBits, num_list):
+
+    value = 0
+
+    # Loop through the bits
+    for i in range(numBits):
+        # Debug code
+        # print(f"Looking at Bit {i}")
+        # print(num_list)
+        # print(f"{i}: {num_list[:,i].sum()} - Len List = {len(num_list)}")
+
+        # Check if this colum most common is a 1 or a 0
+        if (num_list[:, i].sum()) >= len(num_list) / 2:
+            # Most common is a 1
+            # print(f"Index {i} is a 1")
+            oneMostCommon = True
+        else:
+            # Most common is a zero
+            # print(f"Index {i} is a 0")
+            oneMostCommon = False
+
+        temp_list = []
+
+        # Loop through each line in the data and see if this
+        # bit index matches the mostCommon value for this index
+        for index, line in enumerate(num_list):
+            if mode == "oxygen":
+                if oneMostCommon == True and line[i] == 1:
+                    temp_list.append(line)
+                elif oneMostCommon == False and line[i] == 0:
+                    temp_list.append(line)
+            else:
+                if oneMostCommon == True and line[i] == 0:
+                    # Zero is least common
+                    temp_list.append(line)
+                elif oneMostCommon == False and line[i] == 1:
+                    # One is least common
+                    temp_list.append(line)
+
+
+        # Replace our main list with the cut down list ready
+        # for the next loop.
+        num_list = np.array(temp_list)
+
+        # Check if we have finished
+        if (len(num_list) == 1):
+            print("SUCCESS")
+
+            value = int(str(''.join(num_list[0].astype(str))), 2)
+            if mode == "oxygen":
+                print(f"  Oxygen = {value}")
+            else:
+                print(f"  CO2 = {value}")
+            break
+
+    return value
 
 
 def main():
@@ -83,7 +138,6 @@ def main():
 
         part1(data, numLines, numBits)
         part2(data, numLines, numBits)
-
 
 if __name__ == "__main__":
     main()
