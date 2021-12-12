36e1e56e0f9e4190a1ff70fbed564d76907292ac
Jon White <white.jon@gmail.com>
2021-12-12 12:28:35 +0000

    Day 11 - Part 2 complete

--- Day_11/day11.py
+++ Day_11/day11.py
@@ -8,15 +8,15 @@ inputfile = os.path.join(dirname, 'input.txt')
 #inputfile = os.path.join(dirname, 'sample.txt')
 
 
-def flash(octopus, flashed, index):
+def flash(octopus, flashed_map, index):
     # print(f" Flashing {index}")
 
     # Check if this index has been flashed before, if so, abort
-    if flashed[index[0]][index[1]] == True:
+    if flashed_map[index[0]][index[1]] == True:
         print(f"    {index} - already flashed")
     else:
         # Set this index to have flashed and set its value to zero
-        flashed[index[0]][index[1]] = True
+        flashed_map[index[0]][index[1]] = True
         octopus[index[0]][index[1]] = 0
 
     # Determine width and height of the grid
@@ -36,21 +36,22 @@ def flash(octopus, flashed, index):
         # Check x and y boundary
         if (new_y in range(height) and new_x in range(width)):
             # Increment the new point if its not been flashed in this round
-            if flashed[new_y][new_x] == False:
+            if flashed_map[new_y][new_x] == False:
                 octopus[new_y][new_x] += 1
 
 
-def part1(octopus, flashed):
+def part1and2(octopus):
     print("Part 1:")
 
     # Counter to track number of flashes
     total_flashes = 0
+    flashed_count = 0
 
-    # Loop for the required number of steps
-    for step in range(1,101):
+    # Loop for maximum 1000 steps
+    for step in range(1,1000):
 
-        # Reset the flash map
-        flashed = np.array(octopus < 0)
+        # Reset the flash map and count
+        flashed_map = np.array(octopus < 0)
 
         # Add one to each element of the array
         octopus = octopus + 1
@@ -64,19 +65,24 @@ def part1(octopus, flashed):
                 break
 
             for index in indices:
-                flash(octopus, flashed, index)
+                flash(octopus, flashed_map, index)
 
         # Count the number of flashes that happened
-        print(f" Step - {step} - Flashed {np.count_nonzero(flashed == True)}")
-        total_flashes += np.count_nonzero(flashed == True)
+        flashed_count = np.count_nonzero(flashed_map == True)
+        print(f" Step - {step} - Flashed {flashed_count}")
+        total_flashes += flashed_count
+
+        # Log the total flashes at step 100 (Part 1 answer)
+        if step == 100:
+            step_100_count = total_flashes
+
+        # If all octopus flashed, then stop and report the answers
+        if flashed_count == 100:
+            print(f"Part 1: Number of flashes = {step_100_count}")
+            print(f"Part 2: Octopus in sync at step {step}")
+            break;
 
     print(f"Total Flashes - {total_flashes}")
-
-    return
-
-
-def part2(data):
-    print("Part 2:")
 
     return
 
@@ -91,11 +97,7 @@ def main():
 
         octopus = np.array([list(map(int, list(line.strip()))) for line in data])
 
-        # Default an array to False for flash status
-        flashed = np.array(octopus < 0)
-
-        part1(octopus, flashed)
-        #part2(data)
+        part1and2(octopus)
 
 
 if __name__ == "__main__":
