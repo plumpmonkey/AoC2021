6992129beab154595c183f3d47e32edbb88c0e87
Jon White <white.jon@gmail.com>
2021-12-13 18:00:52 +0000

    Day 12 Complete - Took me way longer than it should have

--- Day_12/day12.py
+++ Day_12/day12.py
@@ -3,38 +3,78 @@
 import os
 
 dirname = os.path.dirname(__file__)
-#inputfile = os.path.join(dirname, 'input.txt')
-inputfile = os.path.join(dirname, 'sample.txt')
+inputfile = os.path.join(dirname, 'input.txt')
+#inputfile = os.path.join(dirname, 'sample-3.txt')
 
+paths = 0
+
+
+# Walk through the cave map in a recursive fashion.
+# We seem to need to take a copy of the visited caves list to pass into
+# the recursive function as Python seems to have visibility scope of these
+# variables in the recursive function and this caused no end of issues....
+def explore_cave(cave, visited_caves, adjacentDict, part2=False, visited_small_cave_twice=False):
+
+    global paths
+
+    visited_copy= visited_caves.copy()
+
+    visited_copy.append(cave)
+    # print(f"cave = {cave} visited {visited_caves}")
+
+    if cave == 'end':
+        # print(f"Reached end via path {visited_copy}")
+        paths += 1
+        return
+    else:
+
+        # Part 1 logic is that we can only visit lower case caves once
+        if part2 == False:
+            for connection in adjacentDict[cave]:
+                if connection.islower() and (connection != "start") and connection not in visited_caves:
+                    # print(f"Adding lower case cave {connection}")
+                    explore_cave(connection, visited_copy, adjacentDict)
+                elif connection.isupper():
+                    # print(f"Adding uppercase cave {connection}")
+                    explore_cave(connection, visited_copy, adjacentDict)
+
+        # Part 2 logic is that we can visit ONE lower case cave twice. Pass through a boolean
+        # detailing if we enter a lower cave for the second time.
+        # Once this is set, we cant add a lower case cave thats in the visited list again.
+        # Otherwise the logic is the same as before.
+        else:
+            for connection in adjacentDict[cave]:
+                if connection.islower() and (connection != "start"):
+                    if (connection not in visited_caves):
+                        explore_cave(connection, visited_copy, adjacentDict, part2=True, visited_small_cave_twice=visited_small_cave_twice)
+                    elif (connection in visited_caves) and (visited_small_cave_twice == False):
+                        explore_cave(connection, visited_copy, adjacentDict, part2=True, visited_small_cave_twice=True)
+
+                # Upper case cave. Just add it.
+                elif connection.isupper():
+                    explore_cave(connection, visited_copy, adjacentDict, part2=True, visited_small_cave_twice=visited_small_cave_twice)
 
 def part1(adjacentDict):
     print("Part 1:")
 
     print(adjacentDict)
 
-    paths = 0
-    caves_to_vist = [('start')]
     visited_caves = []
 
-    while caves_to_vist:
-        cave = caves_to_vist.pop(0)
-        if cave == 'end':
-            print(f"Reached end via path {visited_caves}")
-            paths += 1
-            break
-        else:
-            if cave not in visited_caves:
-                visited_caves.append(cave)
+    explore_cave('start', visited_caves, adjacentDict)
 
-            for connection in adjacentDict[cave]:
-                caves_to_vist.append(connection)
-
+    print(f"Part 1: Number of Paths = {paths}")
     return
 
 
-def part2(data):
+def part2(adjacentDict):
     print("Part 2:")
 
+    visited_caves = []
+
+    explore_cave('start', visited_caves, adjacentDict, part2=True)
+
+    print(f"Part 2: Number of Paths = {paths}")
     return
 
 
@@ -50,7 +90,7 @@ def main():
         adjacentDict = dict()
 
         for line in data:
-            cave1, cave2 = line.strip().split("-")
+            cave1, cave2 = line.split("-")
 
             # Set up the dictonary for cave1 -> cave2
             if cave1 in adjacentDict:
@@ -64,8 +104,8 @@ def main():
             else:
                 adjacentDict[cave2] = [cave1]
 
-
         part1(adjacentDict)
+        part2(adjacentDict)
 
 
 if __name__ == "__main__":
